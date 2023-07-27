6ed04600695d17ba81675f2f712df8984df234c5
Donya <thisis2nya@gmail.com>
2023-07-27 22:49:00 +0330

    [UI] create 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="min-h-screen flex flex-col items-center justify-center gap-2">
+    <h1 class="text-5xl font-bold text-purple-900">Page Not Found!</h1>
+    <nuxt-link
+      to="/"
+      class="text-purple-900 border-0 hover:border-b-[1px] border-b-purple-900 transition-all"
+      >Back to Home Page</nuxt-link
+    >
+  </main>
+</template>
+
+<style scoped>
+main {
+  background: rgb(143, 12, 213);
+  background: linear-gradient(
+    90deg,
+    rgba(143, 12, 213, 1) 0%,
+    rgba(217, 29, 253, 1) 50%,
+    rgba(255, 56, 247, 1) 100%
+  );
+}
+</style>
